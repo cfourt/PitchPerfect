2a4029d28f671f29d541456362b0eb0a01d64dbe
Jeremy Silver <jeremy@udacity.com>
2014-12-09 18:11:10 -0800

    Fix bug in prepareForSegue

--- Pitch Perfect/RecordSoundsViewController.swift
+++ Pitch Perfect/RecordSoundsViewController.swift
@@ -66,9 +66,10 @@ class RecordSoundsViewController: UIViewController, AVAudioRecorderDelegate {
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         if segue.identifier == "stopRecording"{
             let playSoundsVC:PlaySoundsViewController = segue.destinationViewController as PlaySoundsViewController
-        }
-        let data = sender as RecordedAudio
-        playSoundsVC.receivedAudio = data
+        
+            let data = sender as RecordedAudio
+            playSoundsVC.receivedAudio = data
+            }
     }
     
     
